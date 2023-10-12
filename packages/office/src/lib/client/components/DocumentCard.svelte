3dca3a3711ea516cca4984eb0b92f17b1a6a192b
Justin Greywolf <jgreywolf@users.noreply.github.com>
2023-10-12 09:43:09 -0700

    styking

--- packages/office/src/lib/client/components/DocumentCard.svelte
+++ packages/office/src/lib/client/components/DocumentCard.svelte
@@ -65,7 +65,7 @@
 </script>
 
 <div class="space-y-4">
-  <div class="flex flex-col space-y-2 py-2">
+  <div class="flex flex-col space-y-2">
     <div data-testid="diagram-title" class="text-left">
       {diagram.title || 'Untitled Diagram'}
     </div>
